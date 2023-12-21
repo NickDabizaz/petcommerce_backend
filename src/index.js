faf95452e68a03812092baa5cee53c196ee4715e
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2023-12-21 07:53:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.static('public', {
   },
 }));
 const corsOptions = {
-  origin: ["https://petcommerce.site","http://localhost:3576"], // Mengizinkan akses dari alamat ini
+  origin: ["https://petcommerce.site","http://localhost:5173"], // Mengizinkan akses dari alamat ini
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
